c125d97c2c881a3a978a73638853fd34cd911b3f
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-03-14 18:52:39 +0000

    Implement simple offer functionality, Step2

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,17 +1,25 @@
 object ShoppingCart {
+  import scala.math.Integral.Implicits._
   def main(args: Array[String]) = {
-
+    val totalPrice = checkout(args)
+    println(s"checkout: $totalPrice")
   }
 
   def checkout(items: Array[String]) =
-    items.foldLeft(0.0) {(sum, i) =>
-      sum + (
-        if (i == "orange")
-         0.25
-        else if (i == "apple")
-         0.6
-        else
-          0.0 // all other items are free
-        )
-    }
+    items.groupBy(identity)
+      .mapValues(_.size).toSeq
+        .foldLeft(0.0) {
+        (b:Double,c:(String,Int)) =>
+          (c match  {
+            case ("apple", count) => {
+              val (quotient, remainder) = count /% 2
+              (quotient + remainder) * 0.6
+            }
+            case ("orange", count) =>  {
+              val (quotient, remainder) = count /% 3
+              quotient * 1.2  + remainder * 0.6
+            }
+            case _ => 0.0 // everything else is free
+          }) + b
+        }
 }
