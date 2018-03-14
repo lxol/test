1c8ab206118ce014fba5f78755dde4967e2a5845
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-03-14 17:50:00 +0000

    Add checkout method to implement Step1

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,4 +1,17 @@
 object ShoppingCart {
   def main(args: Array[String]) = {
+
   }
+
+  def checkout(items: Array[String]) =
+    items.foldLeft(0.0) {(sum, i) =>
+      sum + (
+        if (i == "orange")
+         0.25
+        else if (i == "apple")
+         0.6
+        else
+          0.0 // all other items are free
+        )
+    }
 }
