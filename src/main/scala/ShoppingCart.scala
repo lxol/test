63c994102a695361bff8c438f5305b557f77afaf
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-03-20 12:45:32 +0000

    Fix 2 checkout bugs.
    - the result should be rounded to 2 decimal places
    - wrong cost of apple and oranges

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,25 +1,33 @@
 object ShoppingCart {
+
   import scala.math.Integral.Implicits._
+  import scala.math.BigDecimal
+
+  val AppleCost = 0.6
+  val OrangeCost = 0.25
+
   def main(args: Array[String]) = {
     val totalPrice = checkout(args)
     println(s"checkout: $totalPrice")
   }
 
   def checkout(items: Array[String]) =
-    items.groupBy(identity)
-      .mapValues(_.size).toSeq
+    BigDecimal(
+      items.groupBy(identity)
+        .mapValues(_.size).toSeq
         .foldLeft(0.0) {
-        (b:Double,c:(String,Int)) =>
-          (c match  {
-            case ("apple", count) => {
-              val (quotient, remainder) = count /% 2
-              (quotient + remainder) * 0.6
-            }
-            case ("orange", count) =>  {
-              val (quotient, remainder) = count /% 3
-              quotient * 1.2  + remainder * 0.6
-            }
-            case _ => 0.0 // everything else is free
-          }) + b
+          (b: Double, c: (String, Int)) =>
+            (c match {
+              case ("apple", count) => {
+                val (quotient, remainder) = count /% 2
+                (quotient + remainder) * AppleCost
+              }
+              case ("orange", count) => {
+                val (quotient, remainder) = count /% 3
+                quotient * 2 * OrangeCost + remainder * OrangeCost
+              }
+              case _ => 0.0 // everything else is free
+            }) + b
         }
+    ).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
 }
