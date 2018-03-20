b81fb9c36fc7690cea9ac61a02ab697d327b504a
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-03-20 16:02:07 +0000

    Fix decimal scale.

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,20 +1,25 @@
 object ShoppingCart {
 
+  import scala.math.BigDecimal
+
   def main(args: Array[String]) = {
 
   }
+
   val OrangeCost = 0.25
   val AppleCost = 0.6
 
   def checkout(items: Array[String]) =
-    items.foldLeft(0.0) {(sum, i) =>
-      sum + (
-        if (i == "orange")
-         OrangeCost
-        else if (i == "apple")
-         AppleCost
-        else
-          0.0 // all other items are free
-        )
-    }
+    BigDecimal(
+      items.foldLeft(0.0) { (sum, i) =>
+        sum + (
+          if (i == "orange")
+            OrangeCost
+          else if (i == "apple")
+            AppleCost
+          else
+            0.0 // all other items are free
+          )
+      }
+    ).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
 }
